5bfa410cb679074a821611f59448e1406599dbc0
Vincent Batts <vbatts@hashbangbash.com>
2011-06-17 15:59:31 -0400

    letting the output run off the list of packages too

--- main.cpp
+++ main.cpp
@@ -16,16 +16,13 @@ int main()
 	packages = sys.get_installed_packages();
 	scripts = sys.get_installed_scripts();
 
-	//for (package_vec::iterator p = packages.begin(); p != packages.end() ; p++)
-	//{
-	//	cout << p->name << endl;
-	//}
+	for (package_vec::iterator p = packages.begin(); p != packages.end() ; p++)
+	{
+		cout << p->name << endl;
+	}
 
 	cout << "Packages: " << packages.size() << endl;
 	cout << "Scripts: " << scripts.size() << endl;
-
-	//show_int_sort();
-	//show_string_sort();
 
 	return 0;
 }
